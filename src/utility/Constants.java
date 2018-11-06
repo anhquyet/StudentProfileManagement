dc95fbf24536d2f067798e90e147cedd98500840
hacunamatata97 <qwerty.lynkaa@gmail.com>
2018-11-06 16:18:50 +0700

    Create entity

---
+++ src/utility/Constants.java
@@ -0,0 +1,21 @@
+package utility;
+
+/**
+ * Provide a class to store:
+ * <ul>
+ * <li>HTML parameters
+ * <li>Session attributes
+ * <li>Constant values used in project
+ * </ul>
+ *
+ */
+public final class Constants {
+
+	// HTML parameters
+	public static final String HTML_EMAIL = "email";
+	public static final String HTML_PASSWORD = "password";
+	
+	// Session attributes
+	public static final String SESSION_EMAIL = "email";
+
+}
